1b4597c7f8004580531b176fd2a058a7021859ba
Martin Dransfield <mdransfield@gmail.com>
2013-03-02 17:19:35 +0000

    Changed to use CHECK-TYPE instead of ERROR.

--- fibonacci.lisp
+++ fibonacci.lisp
@@ -9,15 +9,13 @@
 (defun fib (n)
   "Return the Nth number in the fibonacci sequence.
 Defined for n >= 0."
-  (unless (<= 0 n)
-    (error "FIB is only defined for positive n."))
-  (round (/ (expt phi n) sqrt5)))
+  (check-type n (integer 0 *) "A positive integer")
+  (values (round (/ (expt phi n) sqrt5))))
 
 (defun fibs (n)
   "Return the first N numbers in the fibonacci sequence.
 Defined for n >= 0."
-  (unless (<= 0 n)
-    (error "FIBS is only defined for positive n."))
+  (check-type n (integer 0 *) "A positive integer")
   (loop for i from 0 below n
 	for n2 = 0 then n1
 	for n1 = 1 then x
